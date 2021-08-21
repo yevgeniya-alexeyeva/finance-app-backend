06e017fae74c6a9c6f5b62a22d3dbec62a026254
unknown <you@example.com>
2021-08-21 13:45:22 +0300

    setups server

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const transactionsRouter = require('./routes/api/transactions');
+const usersRouter = require('./routes/api/users');
+
+const app = express();
+
+// Middlewares
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing
+app.use('/finapp-api/transactions', transactionsRouter);
+app.use('/finapp-api/users', usersRouter);
+app.use((_, res) => {
+  res.status(404).json({ status: 'error', code: '404', message: 'Not found' });
+});
+
+// Error handler
+app.use((err, _, res, __) => {
+  err.status = err.status || 500;
+  res.status(err.status).json({
+    status: err.status === 500 ? 'fail' : 'error',
+    code: err.status,
+    message: err.message,
+  });
+});
+
+module.exports = app;
---
+++ bin/server.js
@@ -0,0 +1,13 @@
+const app = require('../app');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  console.log('Server terminated');
+  process.exit(1);
+});
