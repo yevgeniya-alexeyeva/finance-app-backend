ad8c83b244b0d248b889597e50a139ed33013335
Andrii Chornyi <andchorniy@gmail.com>
2021-08-27 14:15:53 +0200

    Home Tab
    backend :  endpoint get all user transaktion
    front: fetching transactions

---
+++ controllers/transactions/getTransactions.js
@@ -0,0 +1,24 @@
+const { transactions: service } = require('../../services');
+const getTransactions = async (req, res, next) => {
+  const { _id: userId } = req.user;
+  
+  try {
+    const result = await service.getAll(userId);
+    if (!result) {
+      res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: "No such user's collection",
+      });
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getTransactions;
